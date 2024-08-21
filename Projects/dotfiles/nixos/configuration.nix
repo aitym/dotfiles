b36c328f13a94a14ce66bb816d87ea745fe2ec5f
Aitym Ramazanov <ramazanov.aitym@gmail.com>
2024-08-22 00:56:34 +0500

    feat: run . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" command

--- Projects/dotfiles/nixos/configuration.nix
+++ Projects/dotfiles/nixos/configuration.nix
@@ -99,6 +99,7 @@
       programs.bash = {
         enable = true;
         bashrcExtra = ''
+          . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
           alias config='/run/current-system/sw/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
           /run/current-system/sw/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME config status.showUntrackedFiles no
           eval "$(ssh-agent -s)"
