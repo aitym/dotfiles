a0283fb9a5a883f75f03d8dc05f946ae218084ee
Aitym Ramazanov <ramazanov.aitym@gmail.com>
2024-08-22 00:59:34 +0500

    ref: remove comments

--- Projects/dotfiles/nixos/flake.nix
+++ Projects/dotfiles/nixos/flake.nix
@@ -1,14 +1,9 @@
 {
-  description = "A very basic flake";
-
+  description = "My flake config";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
-
   outputs = { self, nixpkgs }: {
-    #packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    #packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
     nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
       modules = [
         ./configuration.nix
