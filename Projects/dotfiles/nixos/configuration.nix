622195e899b4a77d7a4a304c9d288cc28df2b701
Aitym Ramazanov <ramazanov.aitym@gmail.com>
2024-08-24 05:47:49 +0500

    fix: rename git folder to .dotfiles

--- Projects/dotfiles/nixos/configuration.nix
+++ Projects/dotfiles/nixos/configuration.nix
@@ -62,8 +62,8 @@ in {
         enable = true;
         bashrcExtra = ''
           . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-          alias config='/run/current-system/sw/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
-          /run/current-system/sw/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME config status.showUntrackedFiles no
+          alias config='/run/current-system/sw/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+          /run/current-system/sw/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config status.showUntrackedFiles no
           eval "$(ssh-agent -s)"
           ssh-add ~/.ssh/personal_key
         '';
