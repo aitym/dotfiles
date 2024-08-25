7f4984143ad8e3c01d1c7f89d74c3cecd04586fb
Aitym Ramazanov <ramazanov.aitym@gmail.com>
2024-08-25 23:01:33 +0500

    fix: separate filesystem config as module

--- Projects/dotfiles/flake.nix
+++ Projects/dotfiles/flake.nix
@@ -23,6 +23,7 @@
         modules = [
           ./modules/locale.nix
           ./modules/bootloader.nix
+          ./modules/filesystem.nix
           ./modules/network.nix
           ./modules/bluetooth.nix
           ./modules/system.nix
--- Projects/dotfiles/modules/system.nix
+++ Projects/dotfiles/modules/system.nix
@@ -3,16 +3,6 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/44c3685b-750c-4a0a-8a33-d3efcf4c90ff";
-    fsType = "ext4";
-  };
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/2B79-9416";
-    fsType = "vfat";
-    options = [ "fmask=0022" "dmask=0022" ];
-  };
-  swapDevices = [];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   time.timeZone = "Asia/Aqtobe";
