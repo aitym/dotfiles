788a144c46da9bbc6d0e4aeed2051d8f66948158
Aitym Ramazanov <ramazanov.aitym@gmail.com>
2024-08-25 23:29:51 +0500

    ref: change order of configs

--- Projects/dotfiles/modules/system.nix
+++ Projects/dotfiles/modules/system.nix
@@ -1,11 +1,11 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{ config, lib, modulesPath, ... }:
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "24.05";
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
