2b9551d6646210d94ac2458c7cfe4c4781fe1194
Aitym Ramazanov <ramazanov.aitym@gmail.com>
2024-08-25 23:33:20 +0500

    fix: move config into packages.nix

--- Projects/dotfiles/modules/system.nix
+++ Projects/dotfiles/modules/system.nix
@@ -4,7 +4,6 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
   system.stateVersion = "24.05";
-  nixpkgs.config.allowUnfree = true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
