f6cd4ff16a7fa7fa18e2b5c4d56d671b22d59b5e
Aitym Ramazanov <ramazanov.aitym@gmail.com>
2024-08-27 18:34:07 +0500

    fix: reset passwords

--- Projects/dotfiles/modules/user.nix
+++ Projects/dotfiles/modules/user.nix
@@ -1,7 +1,15 @@
+{ lib, ... }:
 {
   users.users.aitym = {
     isNormalUser = true;
     description = "Aitym";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
+    initialHashedPassword = "";
   };
+  users.users.root.initialHashedPassword = "";
+  security.sudo = {
+    enable = lib.mkDefault true;
+    wheelNeedsPassword = lib.mkImageMediaOverride false;
+  };
+  services.getty.autologinUser = "aitym";
 }
