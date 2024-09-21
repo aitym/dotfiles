43b8d7cae5ded5fccdfaa012dcdfecda68bc1b72
Aitym Ramazanov <ramazanov.aitym@gmail.com>
2024-09-21 13:10:15 +0500

    feat: install some packages

--- Projects/dotfiles/modules/packages.nix
+++ Projects/dotfiles/modules/packages.nix
@@ -4,5 +4,15 @@
   environment.systemPackages = with pkgs; [
     ungoogled-chromium
     htop
+    discord
+    telegram-desktop
+    libreoffice-qt
+    hunspell
+    hunspellDicts.en_US
+    gnumake
+  ];
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    dart
   ];
 }
--- Projects/dotfiles/modules/system.nix
+++ Projects/dotfiles/modules/system.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   system.stateVersion = "24.05";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
